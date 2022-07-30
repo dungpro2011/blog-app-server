9efdc2d465cc9afa5f4481082b2fa6c34229f728
dungpro2011 <75030627+dungpro2011@users.noreply.github.com>
2022-07-30 15:41:36 +0700

    Change port 9000

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 9000;
 const URL = process.env.MONGODB_URI;
 
 app.use(bodyParser.json());
