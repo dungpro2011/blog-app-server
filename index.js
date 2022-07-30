c034b7b995ff6ebc029d9ffcabe4c28285f658e9
dungpro2011 <75030627+dungpro2011@users.noreply.github.com>
2022-07-30 16:03:46 +0700

    change port and var URL

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 9000;
-const URL = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
+const URL = 'mongodb+srv://admin:dung12345678@cluster0.3yrkzwx.mongodb.net/?retryWrites=true&w=majority';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb'}));
